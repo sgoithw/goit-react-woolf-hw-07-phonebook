9b7b626fab1cc375d1d0d88c40d9fdebb5292dcc
Serhii Rybak <serhiirybak@Serhiis-MacBook-Pro.local>
2024-05-03 18:44:12 +0200

    Added store midelware in order to ignore some redux actions

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,23 @@
 import persistStore from 'redux-persist/es/persistStore';
 import { reducer } from './reducer';
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
 
 const store = configureStore({
   reducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 const persistor = persistStore(store);
